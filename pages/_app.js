3de6271ba6052d994778b9bb48fb1ccb6a468639
Joshua <meshachvijaya@gmail.com>
2022-09-15 13:43:13 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from "react";
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // Pass the data from the state context to every single component
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
